2883ad2d89decdfa14daf9f30f43188328888e89
Oleksii Valuiskyi <al.valuiskyi@gmail.com>
2022-06-16 20:53:34 +0300

    Add @Query for findAllByCarId method

--- src/main/java/org/ivanov/domains/repositories/WorkDoneRepository.java
+++ src/main/java/org/ivanov/domains/repositories/WorkDoneRepository.java
@@ -2,9 +2,11 @@ package org.ivanov.domains.repositories;
 
 import org.ivanov.domains.entities.WorkDone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface WorkDoneRepository  extends JpaRepository<WorkDone, Integer> {
+    @Query("select distinct  wd from WorkDone wd where wd.car.id=:carId")
     List<WorkDone> findAllByCarId(int carId);
 }
